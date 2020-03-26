70069de99d99bcaf5dbde132ae27a9d8a74b577b
Jacob Stanger <jacobcstanger@gmail.com>
2020-03-25 22:06:45 -0600

    creating the skelton of the project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const debug = require('debug')('server');
+const chalk = require('chalk');
+const morgan = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 8282;
+app.use(morgan('dev'));
+
+
+app.get('/', function(req, res) {
+  res.end('hi!');
+});
+
+
+app.listen(PORT, function() {
+  debug(`listening on port ${chalk.cyanBright(PORT)}`);
+});
